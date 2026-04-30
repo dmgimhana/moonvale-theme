52be5fbfd7db103ca0d8c03433a41ffdfb66e7d5
dmgimhana <dmgimhana@gmail.com>
2026-04-30 12:13:26 +0530

    feat: loop all posts

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
-<h1><?php bloginfo('name') ?></h1>
-<h1><?php bloginfo('description') ?></h1>
+<?php 
+    while(have_posts()) {
+    the_post(); 
+?>
+
+    <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+        <?php the_content() ?>
+    <hr>
+
+<?php } ?>
