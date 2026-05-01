793b5ebabc3ed07961b3771af0bd4aa69a33335a
dmgimhana <dmgimhana@gmail.com>
2026-05-01 19:03:49 +0530

    feat: featured images

--- functions.php
+++ functions.php
@@ -13,6 +13,9 @@
     function university_features(){
         // register_nav_menu('headerMenuLocation', 'Header Menu Location');
         add_theme_support('title-tag');
+        add_theme_support('post-thumbnails');
+        add_image_size('professorLandscape', 400, 260, true);
+        add_image_size('professorPortrait', 480, 650, true);
     }
 
     add_action('after_setup_theme', 'university_features');
@@ -26,8 +29,6 @@
             $query -> set('posts_per_page', '-1');
         }
     
-
-
 
         if(!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()){
             $today = date('Ymd');
