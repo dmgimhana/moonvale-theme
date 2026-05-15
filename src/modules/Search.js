48f65943b8486a22365d2e2d9a8220a08c5d82f9
dmgimhana <dmgimhana@gmail.com>
2026-05-15 18:51:53 +0530

    feat: conditional logic within template literal

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -44,20 +44,26 @@ class Search {
 
   getResults() {
     $.getJSON(
-      "http://moonvale-university.local/wp-json/wp/v2/posts?search=" +
+      universityData.root_url +
+        "/wp-json/wp/v2/posts?search=" +
         this.searchField.val(),
       (posts) => {
         this.resultsDiv.html(`
           <h2 class="search-overlay__section-title">General Information</h2>
-          <ul class="link-list min-list">
+        ${
+          posts.length
+            ? '<ul class="link-list min-list">'
+            : "<p>No general information</p>"
+        }
             ${posts
               .map(
                 (item) =>
                   `<li><a href="${item.link}">${item.title.rendered}</a></li>`,
               )
               .join("")}
-          </ul>
+            ${posts.length ? "</ul>" : ""}
           `);
+        this.isSpinnerVisible = false;
       },
     );
   }
