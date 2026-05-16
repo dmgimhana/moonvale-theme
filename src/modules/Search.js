31eddd74f06e2b49b80b2fa0453be37e80858877
dmgimhana <dmgimhana@gmail.com>
2026-05-16 09:42:04 +0530

    feat: combine results

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -49,22 +49,30 @@ class Search {
         "/wp-json/wp/v2/posts?search=" +
         this.searchField.val(),
       (posts) => {
-        this.resultsDiv.html(`
+        $.getJSON(
+          universityData.root_url +
+            "/wp-json/wp/v2/pages?search=" +
+            this.searchField.val(),
+          (pages) => {
+            var combineResults = posts.concat(pages);
+            this.resultsDiv.html(`
           <h2 class="search-overlay__section-title">General Information</h2>
         ${
-          posts.length
+          combineResults.length
             ? '<ul class="link-list min-list">'
             : "<p>No general information</p>"
         }
-            ${posts
+            ${combineResults
               .map(
                 (item) =>
                   `<li><a href="${item.link}">${item.title.rendered}</a></li>`,
               )
               .join("")}
-            ${posts.length ? "</ul>" : ""}
+            ${combineResults.length ? "</ul>" : ""}
           `);
-        this.isSpinnerVisible = false;
+            this.isSpinnerVisible = false;
+          },
+        );
       },
     );
   }
