5fe477e52b7977a90c0fbdaba6967256c12a2723
dmgimhana <dmgimhana@gmail.com>
2026-05-16 10:21:07 +0530

    feat: use asynchronous

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -44,18 +44,21 @@ class Search {
   }
 
   getResults() {
-    $.getJSON(
-      universityData.root_url +
-        "/wp-json/wp/v2/posts?search=" +
-        this.searchField.val(),
-      (posts) => {
-        $.getJSON(
-          universityData.root_url +
-            "/wp-json/wp/v2/pages?search=" +
-            this.searchField.val(),
-          (pages) => {
-            var combineResults = posts.concat(pages);
-            this.resultsDiv.html(`
+    $.when(
+      $.getJSON(
+        universityData.root_url +
+          "/wp-json/wp/v2/posts?search=" +
+          this.searchField.val(),
+      ),
+      $.getJSON(
+        universityData.root_url +
+          "/wp-json/wp/v2/pages?search=" +
+          this.searchField.val(),
+      ),
+    ).then(
+      (posts, pages) => {
+        var combineResults = posts[0].concat(pages[0]);
+        this.resultsDiv.html(`
           <h2 class="search-overlay__section-title">General Information</h2>
         ${
           combineResults.length
@@ -70,9 +73,10 @@ class Search {
               .join("")}
             ${combineResults.length ? "</ul>" : ""}
           `);
-            this.isSpinnerVisible = false;
-          },
-        );
+        this.isSpinnerVisible = false;
+      },
+      () => {
+        this.resultsDiv.html("<p>Unexpected error; please try again.</p>");
       },
     );
   }
